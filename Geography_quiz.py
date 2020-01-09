c13d0bd65f2424e295ff14b583b1fd8593565417
Hrvojka Štrtak <hrvojka.strtak@gmail.com>
2020-01-09 22:31:20 +0100

    correction

--- Geography_quiz.py
+++ Geography_quiz.py
@@ -1,13 +1,14 @@
+import random
+
 
 def quiz():
-    import random
     countries_dict = {"Croatia": "Zagreb", "Spain": "Madrid", "Austria": "Vienna", "Slovenia": "Ljubljana",
                       "Italy": "Rome", "Germany": "Berlin", "France": "Paris", "Hungary": "Budapest"}
 
-    for key in countries_dict.items():
-        key = random.choice(list(countries_dict))
-        question = (input(f'> Game: What is the capital city of {key}? \nUser: ')).title()
-        if question == countries_dict[key]:
+    for country in countries_dict.items():
+        country = random.choice(list(countries_dict))
+        question = (input(f'> Game: What is the capital city of {country}? \nUser: ')).title()
+        if question == countries_dict[country]:
             return f'> Game: This is correct.'
         else:
             return f'> Game: This is not correct.'
--- Make_the_guessing_game_more_modular.py
+++ Make_the_guessing_game_more_modular.py
@@ -13,51 +13,40 @@ def get_top_scores():
     for score_dict in score_list[0:3]:
         top_scores = f'NAME: {str(score_dict["name"])}, ATTEMPTS: {str(score_dict["attempts"])}, ' \
                      f'SECRET NUMBER: {str(score_dict["secret"])}, WRONG GUESSES: {str(score_dict["wrong_guesses"])},' \
-                     f' DATE: {score_dict["date"]}, LEVEL: {str(score_dict["level"])}'
+                     f' DATE: {score_dict["date"]}'
         print(top_scores)
 
 
-def play_game():
-    secret = random.randint(1,30)
+def play_game(level="E"):
+    secret = random.randint(1, 30)
     attempts = 0
     wrong_guesses = []
-    level = input("Choose a level. E for easy, H for hard: ").upper()
     name = input("Name: ")
-
     while True:
         attempts += 1
         guess = int(input("Guess: "))
-
-        def level_easy():
-            if guess > secret:
-                print("Your guess is not correct... try something smaller")
-            else:
-                print("Your guess is not correct... try something bigger")
-
-        def level_hard():
-            if guess != secret:
-                print(f'{guess} is not the secret number.')
-
         if secret == guess:
-            score_list.append({"attempts": attempts, "date": str(datetime.datetime.now()), "name": name, "wrong_guesses": wrong_guesses, "secret": secret, "level": level})
+            score_list.append({"attempts": attempts, "date": str(datetime.datetime.now()), "name": name, "wrong_guesses": wrong_guesses, "secret": secret})
             with open("score_list.txt", "w") as score_file:
                 score_file.write(json.dumps(score_list))
             print("You've guessed it - congratulations! It's number " + str(secret))
             print("Attempts needed: " + str(attempts))
             break
-        elif level == "E":
-            level_easy()
+        elif guess > secret and level == "E":
+            print("Your guess is not correct... try something smaller")
+        elif guess < secret and level == "E":
+            print("Your guess is not correct... try something bigger")
         else:
-            level_hard()
+            print(f'{guess} is not the secret number.')
         wrong_guesses.append(guess)
 
 
 def main():
     while True:
-        selection = input("Would you like to A) play a new game, B) see the best scores, or C) quit?").upper()
-
+        selection = input("Would you like to A) play a new game, B) see the best scores or C) quit?").upper()
         if selection == "A":
-            play_game()
+            level = input("Choose a level. E for easy, H for hard: ").upper()
+            play_game(level=level)
         elif selection == "B":
             get_top_scores()
         else:
