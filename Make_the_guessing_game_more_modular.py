7532389f7ce6ebf5374f4dfbfeaa8aaff1c3e425
Hrvojka Štrtak <hrvojka.strtak@gmail.com>
2020-01-09 23:02:26 +0100

    version_2

--- Make_the_guessing_game_more_modular.py
+++ Make_the_guessing_game_more_modular.py
@@ -13,31 +13,44 @@ def get_top_scores():
     for score_dict in score_list[0:3]:
         top_scores = f'NAME: {str(score_dict["name"])}, ATTEMPTS: {str(score_dict["attempts"])}, ' \
                      f'SECRET NUMBER: {str(score_dict["secret"])}, WRONG GUESSES: {str(score_dict["wrong_guesses"])},' \
-                     f' DATE: {score_dict["date"]}'
+                     f' DATE: {score_dict["date"]}, LEVEL: {str(score_dict["level"])}'
         print(top_scores)
 
 
-def play_game(level="E"):
+def level_easy(guess, secret):
+    if guess > secret :
+        print("Your guess is not correct... try something smaller")
+    else:
+        print("Your guess is not correct... try something bigger")
+
+
+def level_hard(guess, secret):
+    if guess != secret:
+        print(f'{guess} is not the secret number')
+
+
+def play_game():
     secret = random.randint(1, 30)
     attempts = 0
     wrong_guesses = []
+    level = input("Choose a level. E for easy, H for hard: ").upper()
     name = input("Name: ")
     while True:
         attempts += 1
         guess = int(input("Guess: "))
         if secret == guess:
-            score_list.append({"attempts": attempts, "date": str(datetime.datetime.now()), "name": name, "wrong_guesses": wrong_guesses, "secret": secret})
+            score_list.append({"attempts": attempts, "date": str(datetime.datetime.now()), "name": name,
+                               "wrong_guesses": wrong_guesses, "secret": secret, "level": level})
             with open("score_list.txt", "w") as score_file:
                 score_file.write(json.dumps(score_list))
             print("You've guessed it - congratulations! It's number " + str(secret))
             print("Attempts needed: " + str(attempts))
             break
-        elif guess > secret and level == "E":
-            print("Your guess is not correct... try something smaller")
-        elif guess < secret and level == "E":
-            print("Your guess is not correct... try something bigger")
-        else:
-            print(f'{guess} is not the secret number.')
+        elif guess != secret:
+            if level == "E":
+                level_easy(guess=guess, secret=secret)
+            else:
+                level_hard(guess=guess, secret=secret)
         wrong_guesses.append(guess)
 
 
@@ -45,8 +58,7 @@ def main():
     while True:
         selection = input("Would you like to A) play a new game, B) see the best scores or C) quit?").upper()
         if selection == "A":
-            level = input("Choose a level. E for easy, H for hard: ").upper()
-            play_game(level=level)
+            play_game()
         elif selection == "B":
             get_top_scores()
         else:
